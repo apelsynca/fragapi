0ea8718dffa96de73d923a2ed1794d0f59a2c1d2
braveltd <belicoff0505@gmail.com>
2026-05-07 09:46:32 +0300

    client feat: app page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -8,11 +8,13 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <main>
-      <div className="flex flex-col gap-4 max-w-128 m-auto">
-        <div>
-          <h1 className="font-semibold text-center">Fragment API</h1>
-          <p className="text-center">abc</p>
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col gap-6 max-w-xl pb-20">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold text-center">FragAPI</h1>
+          <p className="text-lg text-center text-gray-100">
+            Весь функционал fragment, без KYC, без 24-х слов, в виде API.
+          </p>
         </div>
 
         <div className="flex justify-center gap-1">
