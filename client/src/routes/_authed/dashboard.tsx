19e28582f7fd050c921c09993a53e4986579bff3
braveltd <belicoff0505@gmail.com>
2026-05-08 20:54:04 +0300

    fix ;asdasjdajs

--- client/src/routes/_authed/dashboard.tsx
+++ client/src/routes/_authed/dashboard.tsx
@@ -4,13 +4,9 @@ import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { TooltipProvider } from '#/components/ui/tooltip'
 import AppSidebar from '#/components/AppSidebar'
 import { SidebarProvider, SidebarTrigger } from '#/components/ui/sidebar'
-import { userMeQueryOptions } from '#/lib/queries'
 
 export const Route = createFileRoute('/_authed/dashboard')({
   component: RouteComponent,
-  loader: async ({ context }) => {
-    await context.queryClient.ensureQueryData(userMeQueryOptions())
-  },
 })
 
 // idk about p-4 globally
