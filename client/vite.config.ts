45ff4365cc7ea0fae004c3d1cb65c76ecb797c31
braveltd <belicoff0505@gmail.com>
2026-05-09 09:26:54 +0300

    fix: idk count

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   server: {
     port: 3000,
-    allowedHosts: true,
   },
   resolve: { tsconfigPaths: true },
   plugins: [tanstackStart(), viteReact(), tailwindcss(), nitro()],
