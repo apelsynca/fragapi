631933b9c4b91730954b585824287a1c2b3b3734
braveltd <belicoff0505@gmail.com>
2026-05-09 15:27:42 +0300

    fix: server tonapi session

--- server/src/tonapi/endpoints.py
+++ server/src/tonapi/endpoints.py
@@ -1,5 +1,9 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.logging import get_logger
 from src.openapi import APITag
+from src.postgres import get_db_session
 from src.routing import APIRouter
 from src.tonapi.schemas import TonAPIWebhookMessage
 from src.tonapi.service import tonapi as tonapi_service
@@ -10,8 +14,12 @@ log = get_logger()
 
 
 @router.post("/webhook")
-async def tonapi_webhook(message: TonAPIWebhookMessage) -> None:
+async def tonapi_webhook(
+    message: TonAPIWebhookMessage, session: AsyncSession = Depends(get_db_session)
+) -> None:
     log.debug("Tonapi webhook message", message=message)
 
     if message.event_type == "account_tx":
-        await tonapi_service.process_webhook_account_tx_message(message=message)
+        await tonapi_service.process_webhook_account_tx_message(
+            session=session, message=message
+        )
