d95d2885ef5c78cc976f511b5b23c481af5d4893
braveltd <belicoff0505@gmail.com>
2026-05-14 19:15:24 +0300

    fix: host now env variable

--- server/src/config.py
+++ server/src/config.py
@@ -74,12 +74,13 @@ class Settings(BaseSettings):
     def redis_url(self) -> str:
         return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
 
+    AMQP_HOST: str = "127.0.0.1"
     AMQP_USER: str = "guest"
     AMQP_PWD: str = "guest"
 
     @property
     def amqp_url(self) -> str:
-        return f"amqp://{self.AMQP_USER}:{self.AMQP_PWD}@localhost:5672/"
+        return f"amqp://{self.AMQP_USER}:{self.AMQP_PWD}@{self.AMQP_HOST}:5672/"
 
     def get_postgres_dsn(self, driver: Literal["asyncpg", "psycopg2"]) -> str:
         return str(
