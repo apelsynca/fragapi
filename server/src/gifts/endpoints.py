e89b140876a61bca6b667a9dbbc60374ed30c88d
braveltd <belicoff0505@gmail.com>
2026-05-15 10:05:51 +0300

    feat: only gifts

---
+++ server/src/gifts/endpoints.py
@@ -0,0 +1,41 @@
+from fastapi import Depends
+
+from src.auth.dependencies import Authenticator
+from src.auth.scope import Scope
+from src.gifts.service import gift as gift_service
+from src.models.users import User
+from src.openapi import APITag
+from src.routing import APIRouter
+from src.thermos.schemas import GiftModel
+from src.thermos.service import thermos as thermos_service
+
+router = APIRouter(
+    prefix="/gifts",
+    dependencies=[
+        Depends(Authenticator(allowed_subjects={User}, required_scopes={Scope.api}))
+    ],
+    tags=["gifts", APITag.public],
+)
+
+
+@router.get(
+    "/{short_name}/short-models",
+    description="List gift models short names by short_name",
+)
+async def get_gift_models_short_names(short_name: str) -> list[str]:
+    return await gift_service.get_models_by_shortname(short_name)
+
+
+@router.get("/{short_name}/models", description="Get gift collection models")
+async def get_collection_models(short_name: str) -> list[GiftModel]:
+    return await thermos_service.get_collection_models(short_name)
+
+
+@router.get("/{short_name}/models/{name}", description="Get gift collection model")
+async def get_model(
+    short_name: str,
+    name: str,
+) -> GiftModel:
+    return await thermos_service.find_collection_model(
+        short_name=short_name, model=name
+    )
