885b5f151b950353c787f9e881ea555a25f17ab3
braveltd <belicoff0505@gmail.com>
2026-05-20 11:48:23 +0300

    client feat: stats on the main page

--- client/src/components/DashboardStats.tsx
+++ client/src/components/DashboardStats.tsx
@@ -1,7 +1,11 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import DashboardStatsCard from './DashboardStatsCard'
 import BalanceTopUp from './BalanceTopUp'
-import { tonRateQueryOptions, userMeQueryOptions } from '~/lib/queries'
+import {
+  tonRateQueryOptions,
+  transactionsStatsQueryOptions,
+  userMeQueryOptions,
+} from '~/lib/queries'
 import { useTranslation } from 'react-i18next'
 
 export default function DashboardStats() {
@@ -9,6 +13,9 @@ export default function DashboardStats() {
 
   const { data: user } = useSuspenseQuery(userMeQueryOptions())
   const { data: tonRate } = useSuspenseQuery(tonRateQueryOptions())
+  const { data: transactionStats } = useSuspenseQuery(
+    transactionsStatsQueryOptions(),
+  )
 
   return (
     <div className="flex flex-col items-center md:grid md:grid-cols-4 gap-1 md:gap-2.5">
@@ -18,28 +25,27 @@ export default function DashboardStats() {
         fiatAmount={user.balance * tonRate}
         after={<BalanceTopUp />}
       />
+      <DashboardStatsCard
+        name={t('stats.spend')}
+        amount={transactionStats.totalSpend || 0}
+        fiatAmount={transactionStats.totalSpend * tonRate}
+        percent={0}
+        description={t('stats.all_time')}
+      />
+      <DashboardStatsCard
+        name={t('stats.stars_spend')}
+        amount={transactionStats.starsTotalSpend || 0}
+        fiatAmount={transactionStats.starsTotalSpend * tonRate}
+        percent={0}
+        description={t('stats.all_time')}
+      />
+      <DashboardStatsCard
+        name={t('stats.premium_spend')}
+        amount={transactionStats.premiumTotalSpend || 0}
+        fiatAmount={transactionStats.premiumTotalSpend * tonRate}
+        percent={0}
+        description={t('stats.all_time')}
+      />
     </div>
   )
 }
-
-// <DashboardStatsCard
-//   name={t('stats.spend')}
-//   amount={transactionStats.monthlySpend || 0}
-//   fiatAmount={transactionStats.totalSpent * tonRate}
-//   percent={0}
-//   description={t('stats.last_30days')}
-// />
-// <DashboardStatsCard
-//   name={t('stats.stars_spend')}
-//   amount={transactionStats.starsMonthlySpend || 0}
-//   fiatAmount={transactionStats.starsMonthlySpend * tonRate}
-//   percent={0}
-//   description={t('stats.last_30days')}
-// />
-// <DashboardStatsCard
-//   name={t('stats.premium_spend')}
-//   amount={transactionStats.premiumMonthlySpend || 0}
-//   fiatAmount={transactionStats.premiumMonthlySpend * tonRate}
-//   percent={0}
-//   description={t('stats.last_30days')}
-// />
