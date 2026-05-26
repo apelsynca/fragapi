8bcce1dba68079bacc078d4dfc0e8daf7ca3bc03
apelsynca <apelsynca@proton.me>
2026-05-26 12:54:38 +0300

    feat: changed UA

--- server/scripts/raw_frag_request.py
+++ server/scripts/raw_frag_request.py
@@ -14,7 +14,7 @@ async def main():
         headers={
             "Origin": "https://fragment.com",
             "Referer": "https://fragment.com/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/150.0",
             "Accept": "application/json, text/javascript, */*; q=0.01",
             "Accept-Encoding": "gzip, deflate, br, zstd",
             "Accept-Language": "en-US,en;q=0.9",
--- server/src/integrations/fragment/rest_client.py
+++ server/src/integrations/fragment/rest_client.py
@@ -31,7 +31,7 @@ class FragmentRestClient:
 
         self._client: BaseClient = HttpxClient(
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Moonflower/1.0",
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/150.0",
                 "Accept": "application/json, text/javascript, */*; q=0.01",
                 "Accept-Encoding": "gzip, deflate, br, zstd",
                 "Accept-Language": "en-US,en;q=0.9",
