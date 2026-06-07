15b0b3cddca722fd47c3dbe5ef7ff494ed84f21b
apelsynca <apelsynca@proton.me>
2026-06-07 08:17:14 +0300

    refactor: moved generate_api_token, api token prefix

--- server/src/kit/utils.py
+++ server/src/kit/utils.py
@@ -1,4 +1,3 @@
-import secrets
 import uuid
 from datetime import UTC, datetime
 
@@ -9,8 +8,3 @@ def utc_now() -> datetime:
 
 def generate_uuid() -> uuid.UUID:
     return uuid.uuid4()
-
-
-def generate_api_token() -> str:
-    """Create new api token string"""
-    return secrets.token_urlsafe(32)
