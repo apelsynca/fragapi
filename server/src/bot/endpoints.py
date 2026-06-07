bcaeabeba07f9239a7b808717f64eddbebd75068
apelsynca <apelsynca@proton.me>
2026-06-07 20:24:01 +0300

    feat: telegram bot secret token validation

--- server/src/bot/endpoints.py
+++ server/src/bot/endpoints.py
@@ -1,14 +1,19 @@
+import structlog
 from aiogram import Bot
 from aiogram.types import Update
 from fastapi import Depends, Request, Response
 
 from src.bot.handlers import dispatcher
 from src.config import settings
+from src.exceptions import Unauthorized
+from src.logging import Logger
 from src.openapi import APITag
 from src.postgres import AsyncSession, get_db_session
 from src.routing import APIRouter
 
 router = APIRouter(tags=[APITag.private])
+
+log: Logger = structlog.get_logger()
 
 
 @router.post(settings.BOT_WEBHOOK_PATH)
@@ -19,6 +24,13 @@ async def telegram_bot_webhook(
         bot: Bot = request.state.bot
     except AttributeError:
         raise RuntimeError("Bot not in state, fixit")
+
+    if (
+        request.headers["X-Telegram-Bot-Api-Secret-Token"]
+        != settings.BOT_WEBHOOK_SECRET_TOKEN
+    ):
+        log.warning("telegram bot webhook wrong secret token")
+        raise Unauthorized("Wrong secret token")
 
     update = Update.model_validate(await request.json(), context={"bot": bot})
     await dispatcher.feed_update(bot=bot, update=update, session=session)
