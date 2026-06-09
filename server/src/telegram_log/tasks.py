3657f08020b94debd030d6ff9bbbff0848cb41c7
apelsynca <apelsynca@proton.me>
2026-06-09 05:21:27 +0300

    feat: basic telegram log sender

--- server/src/telegram_log/tasks.py
+++ server/src/telegram_log/tasks.py
@@ -1,2 +1,12 @@
-async def telegram_log_send():
-    pass
+from src.worker import broker
+
+from .sender import telegram_log_sender
+
+
+@broker.task
+async def telegram_log_send(
+    chat_id: int, text: str, with_notification: bool = False
+) -> None:
+    await telegram_log_sender.send(
+        chat_id=chat_id, text=text, with_notification=with_notification
+    )
