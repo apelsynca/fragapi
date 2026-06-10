5d94577959b6a527f9bfc48e3db33f572ef6641b
apelsynca <apelsynca@proton.me>
2026-06-10 11:41:33 +0300

    refactor: dashboard separator

--- client/src/routes/dashboard/route.tsx
+++ client/src/routes/dashboard/route.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from 'sonner'
+import { useTranslation } from 'react-i18next'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar'
 import { userMeOptions } from '~/lib/queries'
@@ -6,6 +7,7 @@ import { TooltipProvider } from '~/components/ui/tooltip'
 import DashboardProviders from '~/components/Dashboard/DashboardProviders'
 import Landing from '~/components/Landing'
 import AppSidebar from '~/components/AppSidebar'
+import { Separator } from '~/components/ui/separator'
 
 export const Route = createFileRoute('/dashboard')({
   beforeLoad: ({ context }) => {
@@ -27,13 +29,19 @@ export const Route = createFileRoute('/dashboard')({
 })
 
 function DashboardComponent() {
+  const { t } = useTranslation()
+
   return (
     <TooltipProvider>
       <SidebarProvider>
         <DashboardProviders>
           <AppSidebar />
           <main className="w-full relative">
-            <SidebarTrigger className="ml-2 mt-2 absolute" />
+            <div className="flex items-center gap-2 absolute mx-1 my-1">
+              <SidebarTrigger className="px-2 py-2" />
+              <Separator orientation="vertical" />
+              <h2 className="text-base font-medium">{t('dashboard')}</h2>
+            </div>
             <div className="mt-8 pt-4 px-2 md:px-4 w-full max-w-7xl mx-auto">
               <Outlet />
             </div>
