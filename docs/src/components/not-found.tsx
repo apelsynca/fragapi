1f0d831d011b2b065fa054f2e6df667902cef2ad
apelsynca <apelsynca@proton.me>
2026-06-17 17:18:18 +0300

    docs: toggle language was not changing sidebar links

--- docs/src/components/not-found.tsx
+++ docs/src/components/not-found.tsx
@@ -1,10 +1,12 @@
-import { baseOptions } from '@/lib/layout.shared';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { DefaultNotFound } from 'fumadocs-ui/layouts/home/not-found';
+import { i18n } from "@/lib/i18n";
+import { baseOptions } from "@/lib/layout.shared";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { DefaultNotFound } from "fumadocs-ui/layouts/home/not-found";
 
 export function NotFound() {
+  // TODO: if 'ru' / 'en' present in pathname, use them, otherwise default language
   return (
-    <HomeLayout {...baseOptions()}>
+    <HomeLayout {...baseOptions(i18n.defaultLanguage)}>
       <DefaultNotFound />
     </HomeLayout>
   );
