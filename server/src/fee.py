6f1c72a21a0f25a885ec298b7c36f41ea90713be
apelsynca <apelsynca@proton.me>
2026-07-01 13:52:41 +0300

    feat: raw commission admin stats, unique users, deposits, before fee function

--- server/src/fee.py
+++ server/src/fee.py
@@ -4,7 +4,13 @@ TON_FEE = 0.00582  # 0.00582 TON in general
 
 
 def after_fee(amount: float) -> float:
-    return amount + (amount * settings.API_PRICE_MARKUP)
+    return amount * (1 + settings.API_PRICE_MARKUP)
+
+
+def approx_before_fee(amount: float) -> float:
+    if amount <= TON_FEE:
+        return 0
+    return amount / (1 + settings.API_PRICE_MARKUP) - TON_FEE
 
 
 def after_ton_network_fee(amount: float) -> float:
--- server/tests/backoffice/transactions/test_tasks.py
+++ server/tests/backoffice/transactions/test_tasks.py
@@ -13,6 +13,7 @@ from src.backoffice.transactions.tasks import (
     transactions_log_daily_stats,
 )
 from src.enums import TransactionReason
+from src.fee import approx_before_fee
 from src.kit.utils import utc_now
 from src.models import Transaction, User
 from tests.fixtures.database import SaveFixture
@@ -81,8 +82,11 @@ async def test_log_daily_stats_right_text(
         text=DAILY_LOG_TEXT.format(
             date=yesterday_dt.date().strftime("%m-%d"),
             amount=7.75,
+            raw_commission_amount=approx_before_fee(7.75),
             transactions_count=2,
             unique_users=2,
+            new_users_count=0,
+            deposits_count=0,
         ),
         with_notification=False,
     )
@@ -102,8 +106,11 @@ async def test_log_empty_text(
         text=DAILY_LOG_TEXT.format(
             date=yesterday_dt.date().strftime("%m-%d"),
             amount=0,
+            raw_commission_amount=0,
             transactions_count=0,
             unique_users=0,
+            new_users_count=0,
+            deposits_count=0,
         ),
         with_notification=False,
     )
@@ -127,9 +134,41 @@ async def test_log_right_unique_users(
         telegram_log_send,
         text=DAILY_LOG_TEXT.format(
             date=yesterday_dt.date().strftime("%m-%d"),
-            amount=(5 + 3) * U_COUNT,
+            amount=8 * U_COUNT,
+            raw_commission_amount=approx_before_fee(8 * U_COUNT),
             transactions_count=U_COUNT * 2,
             unique_users=U_COUNT,
+            new_users_count=0,
+            deposits_count=0,
+        ),
+        with_notification=False,
+    )
+
+
+@pytest.mark.asyncio
+async def test_log_right_new_users(
+    save_fixture: SaveFixture, enqueue_task_mock: MagicMock, session: AsyncSession
+) -> None:
+    yesterday_dt = utc_now() - timedelta(days=1)
+
+    for _ in range(3):  # create unique users
+        user = User(
+            first_name=rstr("Mock"), username=rstr("test_"), created_at=yesterday_dt
+        )
+        await save_fixture(user)
+
+    await transactions_log_daily_stats(session)
+
+    enqueue_task_mock.assert_called_once_with(
+        telegram_log_send,
+        text=DAILY_LOG_TEXT.format(
+            date=yesterday_dt.date().strftime("%m-%d"),
+            amount=0,
+            raw_commission_amount=0,
+            transactions_count=0,
+            unique_users=0,
+            new_users_count=3,
+            deposits_count=0,
         ),
         with_notification=False,
     )
