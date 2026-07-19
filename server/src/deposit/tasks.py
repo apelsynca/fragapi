bdc6d7cdd73b79a5419f940500ffac37da02fea8
apelsynca <apelsynca@proton.me>
2026-07-19 19:11:19 +0300

    feat: created basic clearing task

---
+++ server/src/deposit/tasks.py
@@ -0,0 +1,21 @@
+from datetime import timedelta
+
+from sqlalchemy import update
+
+from src.kit.utils import utc_now
+from src.models import Deposit
+from src.models.deposits import DepositStatus
+from src.postgres import AsyncSession
+from src.worker import worker_task_with_queue_manager
+
+
+@worker_task_with_queue_manager("deposit.set_failed_to_expired_ones")
+async def deposit_set_failed_to_expired_ones(session: AsyncSession) -> None:
+    await session.execute(
+        update(Deposit)
+        .values(status=DepositStatus.failed)
+        .where(
+            Deposit.created_at < utc_now() - timedelta(hours=1),
+            Deposit.status == DepositStatus.pending,
+        )
+    )
