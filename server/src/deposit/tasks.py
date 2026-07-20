09a7dc7d3a40607d6d60813b66f5fc9ed0fc0e95
apelsynca <apelsynca@proton.me>
2026-07-20 07:45:14 +0300

    feat: deposits fail added cron and included into tasks

--- server/src/deposit/tasks.py
+++ server/src/deposit/tasks.py
@@ -6,10 +6,12 @@ from src.kit.utils import utc_now
 from src.models import Deposit
 from src.models.deposits import DepositStatus
 from src.postgres import AsyncSession
-from src.worker import worker_task_with_queue_manager
+from src.worker import broker
 
 
-@worker_task_with_queue_manager("deposit.set_failed_to_expired_ones")
+@broker.task(
+    task_name="deposit.set_failed_to_expired_ones", schedule=[{"cron": "30 * * * *"}]
+)
 async def deposit_set_failed_to_expired_ones(session: AsyncSession) -> None:
     await session.execute(
         update(Deposit)
--- server/src/tasks.py
+++ server/src/tasks.py
@@ -1,10 +1,12 @@
 from src.auth import tasks as auth_tasks
+from src.deposit import tasks as deposit_tasks
 from src.telegram_log import tasks as telegram_log_tasks
 from src.ton_transaction import tasks as ton_transaction_tasks
 from src.transaction import tasks as transaction_tasks
 
 __all__ = [
     "auth_tasks",
+    "deposit_tasks",
     "telegram_log_tasks",
     "ton_transaction_tasks",
     "transaction_tasks",
