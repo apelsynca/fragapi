00bb3c880cfa4262e23d0510060a4f45d8b30886
apelsynca <apelsynca@proton.me>
2026-07-22 10:10:05 +0300

    feat: client landing 1 faq item + faq description

--- client/src/components/Landing/FAQ.tsx
+++ client/src/components/Landing/FAQ.tsx
@@ -5,6 +5,7 @@ import {
   AccordionTrigger,
 } from '#/components/ui/accordion'
 import { Section } from '#/components/ui/section'
+import { siteConfig } from '#/config'
 import { m } from '#/paraglide/messages'
 
 interface FAQItemProps {
@@ -25,8 +26,17 @@ export default function FAQ({
     {
       question: 'Почему FragAPI?',
       answer: (
-        <p className="text-muted-foreground mb-4 max-w-[640px] text-balance">
-          Не нужно KYC, низкая комса 0.5% (не 50%)
+        <p className="text-muted-foreground mb-4 max-w-160 text-balance">
+          Полная автоматизация, Не нужно KYC, низкая коммисия 0.5% (не 50%),
+          открытый исходный код, cтатистика, оповещения и многое другое.
+        </p>
+      ),
+    },
+    {
+      question: 'Берет ли сервис коммисию?',
+      answer: (
+        <p className="text-muted-foreground mb-4 max-w-160 text-balance">
+          Да, 0.5%, тоесть с транзакций на 1000 USD мы снимаем 5 USD
         </p>
       ),
     },
@@ -53,6 +63,12 @@ export default function FAQ({
           </Accordion>
         )}
       </div>
+      <p className="mt-4 text-xs">
+        Смело задавайте интересующие вас вопросы или предложения в наш{' '}
+        <a className="font-medium italic" href={siteConfig.telegramChat}>
+          телеграмм чат!
+        </a>
+      </p>
     </Section>
   )
 }
